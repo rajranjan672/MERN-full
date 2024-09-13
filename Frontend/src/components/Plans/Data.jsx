facd93cc0d3aafe12ee94e8a71eb3b655d188afb
raj.ranjan <raj.ranjan@greatmanagerinstitute.com>
2024-09-13 23:26:00 +0530

    sDG
    df

--- Frontend/src/components/Plans/Data.jsx
+++ Frontend/src/components/Plans/Data.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./card.css";
 import girl from "../../Assets/beautiful-girl-eating-vegetarian-lunch.png";
+import { FitScreen } from "@mui/icons-material";
 
 const Data = () => {
   return (
@@ -11,18 +12,18 @@ const Data = () => {
           <div className="row m-0">
             <div className="col-10 col-sm-10 col-md-5 col-lg-4 mt-5 pt-1 px-0">
               <h1 className="text-white">Explore Your Ideas Here with US</h1>
-              <p>
+              <p className="text-white">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
                 in, suscipit iure ullam aspernatur inventore quidem soluta!
                 Quas, soluta aliquam?
               </p>
-              <button>BUTTON</button>
+              <button className="bt">BUTTON</button>
             </div>
             <div className="col-11 col-sm-11 col-md-6 col-lg-4 girl mt-5">
               <img
                 className="col-12 col-sm-10 col-md-12 col-lg-11"
                 src={girl}
-                height={490}
+                height={FitScreen}
               />
             </div>
 
