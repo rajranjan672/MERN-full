421c986c86eea5f6537a73f888c824bd56bc130d
raj.ranjan <raj.ranjan@greatmanagerinstitute.com>
2024-09-19 18:16:11 +0530

    sdf
    sdf

--- Frontend/src/components/Plans/Data.jsx
+++ Frontend/src/components/Plans/Data.jsx
@@ -4,7 +4,36 @@ import "./card.css";
 import girl from "../../Assets/beautiful-girl-eating-vegetarian-lunch.png";
 import { FitScreen } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
-import sweet from "../../Assets/sweet.jpeg"
+import sweet from "../../Assets/sweet.jpeg";
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Year", "Sales", "Expenses", "Profit"],
+  ["2014", 1000, 400, 200],
+  ["2015", 1170, 460, 250],
+  ["2016", 660, 1120, 300],
+  ["2017", 1030, 540, 350],
+];
+
+export const options = {
+  chart: {
+    // title: "Company Performance",
+    // subtitle: "Sales, Expenses, and Profit: 2014-2017",
+  },
+};
+
+export const data1 = [
+  ["Task", "Hours per Day"],
+  ["Work", 11],
+  ["Eat", 5],
+  ["Sleep", 7], // CSS-style declaration
+];
+
+export const options1 = {
+  title: "My Daily Activities",
+  pieHole: 0,
+  is3D: false,
+};
 
 const Data = () => {
   const [isHovered, setIsHovered] = useState(false);
@@ -54,88 +83,103 @@ const Data = () => {
           </div>
 
           {/* <div className="row"> */}
-          {/* breakfase */}
+          {/* breakfast */}
           <div className="row p-1">
+            <div className="col-12 m-0 my-2 py-2 ">
+              <div className="d-flex justify-content-between">
+                <h3 className=" justify-content-start">Breakfast</h3>
+                <i
+                  className="bi bi-arrow-right alignt-end me-3"
+                  role="button"
+                />
+              </div>
 
-          <div className="card col-12 m-0 my-2 py-2 ">
-            
-            <div className="d-flex justify-content-between">
-
-              <h3 className=" justify-content-start">Breakfast</h3>
-              <i className="bi bi-arrow-right alignt-end me-3" role="button" />
+              <div className=" row justify-content-between">
+                <div className="card col-8 col-sm-12 col-md-4 col-lg-3 image-container mb-4 py-2 px-2">
+                  <img src={sweet} alt="" className="image" />
+                  <div className="overlay">
+                    <h1 className="text-white text-center mt-5">Indian</h1>
+                  </div>
+                </div>
+                <div className="card col-8 col-sm-12 col-md-4 col-lg-3 image-container mb-4 py-2 px-2">
+                  <img src={sweet} alt="" className="image" />
+                  <div className="overlay">
+                    <h1 className="text-white text-center mt-5">Chinese</h1>
+                  </div>
+                </div>
+                <div className="card col-8 col-sm-12 col-md-4 col-lg-3 image-container mb-4 py-2 px-2">
+                  <img src={sweet} alt="" className="image" />
+                  <div className="overlay">
+                    <h1 className="text-white text-center mt-5">Maxican</h1>
+                  </div>
+                </div>
+              </div>
             </div>
-            
-          <div className="row justify-content-between py-2 px-1">
-            
-        <div className="col-3 image-container">
-            <img src={sweet} alt="" className="image" />
-            <div className="overlay">
-                <h1 className='text-white text-center mt-5'>hi</h1>
-            </div>
-        </div>
-        <div className="col-3 image-container">
-            <img src={sweet} alt="" className="image" />
-            <div className="overlay">
-                <h1 className='text-white text-center mt-5'>hi</h1>
-            </div>
-        </div>
-        <div className="col-3 image-container">
-            <img src={sweet} alt="" className="image" />
-            <div className="overlay">
-                <h1 className='text-white text-center mt-5'>hi</h1>
-            </div>
-        </div>
-        
-        </div>
-        </div>
-        </div>
-
-
-        {/* Lunch */}
-
-        
-        <div className="row">
-
-        <div className="card col-12 m-0 py-2 my-4 my-0">
-            
-            <div className="d-flex justify-content-between">
-
-              <h3 className=" justify-content-start">Breakfast</h3>
-              <i className="bi bi-arrow-right alignt-end me-3" role="button" />
-            </div>
-            
-          <div className="row justify-content-between pb-2 px-1">
-            
-        <div className="col-3 image-container">
-            <img src={sweet} alt="" className="image" />
-            <div className="overlay">
-                <h1 className='text-white text-center mt-5'>hi</h1>
-            </div>
-        </div>
-        <div className="col-3 image-container">
-            <img src={sweet} alt="" className="image" />
-            <div className="overlay">
-                <h1 className='text-white text-center mt-5'>hi</h1>
-            </div>
-        </div>
-        <div className="col-3 image-container">
-            <img src={sweet} alt="" className="image" />
-            <div className="overlay">
-                <h1 className='text-white text-center mt-5'>hi</h1>
-            </div>
-        </div>
-        
-        </div>
-        </div>
-        </div>
-
           </div>
-         
+
+          <div className="row justify-content-between px-2">
+          <h3>Food data</h3>
+
+            <div className="card col-11 col-sm-11 col-md-11 col-lg-7 py-2 mb-3">
+            <Chart
+      chartType="Bar"
+      width="100%"
+      height="400px"
+      data={data}
+      options={options}
+    />
+            </div>
+
+            <div className="card col-10 col-sm-11 col-md-11 col-lg-4 justify-content-end mb-3">
+             <Chart
+      chartType="PieChart"
+      width="100%"
+      height="400px"
+      data={data1}
+      options={options1}
+    />
+            </div>
+          </div>
+
+          {/* Lunch */}
+
+          <div className="row p-1">
+            <div className="col-12 m-0 my-2 py-2 ">
+              <div className="d-flex justify-content-between">
+                <h3 className=" justify-content-start">Breakfast</h3>
+                <i
+                  className="bi bi-arrow-right alignt-end me-3"
+                  role="button"
+                />
+              </div>
+
+              <div className=" row justify-content-between">
+                <div className="card col-8 col-sm-12 col-md-4 col-lg-3 image-container mb-4 py-2 px-2">
+                  <img src={sweet} alt="" className="image" />
+                  <div className="overlay">
+                    <h1 className="text-white text-center mt-5">Indian</h1>
+                  </div>
+                </div>
+                <div className="card col-8 col-sm-12 col-md-4 col-lg-3 image-container mb-4 py-2 px-2">
+                  <img src={sweet} alt="" className="image" />
+                  <div className="overlay">
+                    <h1 className="text-white text-center mt-5">Chinese</h1>
+                  </div>
+                </div>
+                <div className="card col-8 col-sm-12 col-md-4 col-lg-3 image-container mb-4 py-2 px-2">
+                  <img src={sweet} alt="" className="image" />
+                  <div className="overlay">
+                    <h1 className="text-white text-center mt-5">Maxican</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
       {/* </div> */}
     </>
   );
 };
 
 export default Data;
-
